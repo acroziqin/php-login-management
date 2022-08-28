4c05f95e8ad6be023d91a42072220813605fa9a3
unknown <achmadchoirurroziqin@gmail.com>
2022-08-28 21:15:04 +0700

    User Registration Controller

---
+++ app/Controller/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace KrisnaBeaute\BelajarPhpMvc\Controller;
+
+use KrisnaBeaute\BelajarPhpMvc\App\View;
+use KrisnaBeaute\BelajarPhpMvc\Config\Database;
+use KrisnaBeaute\BelajarPhpMvc\Exception\ValidationException;
+use KrisnaBeaute\BelajarPhpMvc\Model\UserRegisterRequest;
+use KrisnaBeaute\BelajarPhpMvc\Repository\UserRepository;
+use KrisnaBeaute\BelajarPhpMvc\Service\UserService;
+
+class UserController
+{
+    private UserService $userService;
+
+    public function __construct()
+    {
+        $connection = Database::getConnection();
+        $userRepository = new UserRepository($connection);
+        $this->userService = new UserService($userRepository);
+    }
+
+
+    public function register()
+    {
+        View::render('User/register', [
+            'title' => 'Register new User'
+        ]);
+    }
+
+    public function postRegister()
+    {
+        $request = new UserRegisterRequest();
+        $request->id = $_POST['id'];
+        $request->name = $_POST['name'];
+        $request->password = $_POST['password'];
+
+        try {
+            $this->userService->register($request);
+            View::redirect('users/login');
+        } catch (ValidationException $exception) {
+            View::render('User/register', [
+                'title' => 'Register new User',
+                'error' => $exception->getMessage()
+            ]);
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use KrisnaBeaute\BelajarPhpMvc\App\Router;
+use KrisnaBeaute\BelajarPhpMvc\Config\Database;
 use KrisnaBeaute\BelajarPhpMvc\Controller\HomeController;
+use KrisnaBeaute\BelajarPhpMvc\Controller\UserController;
 
+Database::getConnection('prod');
+
+// Home Controller
 Router::add('GET', '/', HomeController::class, 'index', []);
+
+// User Controller
+Router::add('GET', '/users/register', UserController::class, 'register', []);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
 
 Router::run();
