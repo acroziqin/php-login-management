a970c28efc11590cf68bac33dbd8a01eada9743b
acroziqin <achmadchoirurroziqin@gmail.com>
2022-09-05 12:07:46 +0700

    Unit Test User Profile Update Repository

--- test/Repository/UserRepositoryTest.php
+++ test/Repository/UserRepositoryTest.php
@@ -9,9 +9,12 @@ use PHPUnit\Framework\TestCase;
 class UserRepositoryTest extends TestCase
 {
     private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     protected function setUp(): void
     {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();
 
         $this->userRepository = new UserRepository(Database::getConnection());
         $this->userRepository->deleteAll();
@@ -37,6 +40,25 @@ class UserRepositoryTest extends TestCase
     {
         $user = $this->userRepository->findById("notfound");
         self::assertNull($user);
+    }
+
+    public function testUpdate()
+    {
+        $user = new User();
+        $user->id = "roziqin";
+        $user->name = "Roziqin";
+        $user->password = "rahasia";
+
+        $this->userRepository->save($user);
+
+        $user->name = "Budi";
+        $this->userRepository->update($user);
+
+        $result = $this->userRepository->findById($user->id);
+
+        self::assertEquals($user->id, $result->id);
+        self::assertEquals($user->name, $result->name);
+        self::assertEquals($user->password, $result->password);
     }
 
 
